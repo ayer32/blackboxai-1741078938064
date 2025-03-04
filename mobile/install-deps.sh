0601c0daaf10eb8c3e2df495587202c16540b985
ayer32 <48725957+ayer32@users.noreply.github.com>
2025-03-04 15:59:16 +0545

    Add mobile/install-deps.sh 1741083256160

---
+++ mobile/install-deps.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install required type definitions
+npm install --save-dev @types/node
+npm install --save-dev @types/lottie-react-native
+
+# Install expo type definitions
+npm install --save-dev @types/expo
+npm install --save-dev @types/expo-camera
+npm install --save-dev @types/expo-blur
+
+# Make sure all dependencies are installed
+npm install
